e3234c9771fb5a9676cbea117b4d85964c824fe8
EC2 Default User <ec2-user@ip-172-31-107-37.ec2.internal>
2020-05-19 15:34:04 +0000

    added my lab2.py file

---
+++ lab2.py
@@ -0,0 +1,42 @@
+"""
+Lab 2
+Maria Liu
+
+"""
+#4.1
+my_name = 'Tom'
+print(my_name.upper())
+
+#4.2
+my_id = 123
+print(my_id)
+
+#4.3
+#123 = my_id    #can assign to something literal
+my_id = your_id = 123
+print(my_id)
+print(your_id)
+
+#4.4
+my_id_str = '123'
+print(my_id_str)
+
+#4.5
+#print(my_name + my_id) #cannot add a int
+
+#4.6
+print(my_name + my_id_str)  #can add a string
+
+#4.7
+print(my_name * 3)
+
+#4.8
+print("hello, world. This is my first python string.".split("."))
+
+#4.9
+'''
+message = ’Tom’s id is 123’     #cannot have the ' apostrophe with single quotes
+print (message)
+'''
+message = "Tom’s id is 123"     #can us the apostrophe with double quotes
+print(message)
---
+++ lec2.py
@@ -0,0 +1,35 @@
+"""
+This is my second py code
+
+for my second lecture
+"""
+#print('hello world') #single line comment
+
+#this is my second line comment
+'''
+print(type(123))
+print(type(123.0))
+print(type('123'))
+'''
+#print("Hello World".upper()) #makes it all capital
+#print("Hello World".lower())   #makes it all lowercase
+
+#print ("   hello  world ".strip()) #cut off the spaces before and after a string
+'''
+print("hello world".split("w"))     #will split into 'hello' and 'orld'
+print("hello, world".split(","))    #you can add a comma and split there
+print("hello, world".split())   #by default the string is split by the space
+print("hello, world".split('l'))    #it will split it at every letter 'l'
+
+#print("hello " + "world" + ".")    #space matters
+
+#print(2+3)
+
+my_str = 'hello world'
+print (my_str)
+print (my_str.upper())
+'''
+
+my_int = 2
+my_float = 3.0
+print(my_int + my_float)
