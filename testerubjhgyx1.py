47ce74a0539fd3dd598ff91ea328d759e67a7025
EC2 Default User <ec2-user@ip-172-31-107-37.ec2.internal>
2020-05-28 15:34:07 +0000

    day7

---
+++ testerubjhgyx1.py
@@ -0,0 +1,17 @@
+"""
+Your module description
+"""
+
+i = 1
+dividend = 1
+while i <= 8:
+    dividend = dividend*i
+    i = i + 1
+print(dividend)
+i = 1
+divisor = 1
+while i <= 3:
+    divisor = divisor*i
+    i = i + 1
+print(divisor)
+print(dividend/divisor)
